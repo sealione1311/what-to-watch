91335982c5bddbb13b7cf1e4cd86ac1550a07214
sealione1311 <sealione@gmail.com>
2020-06-13 00:15:20 +0300

    добавляет валидацию props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   return <Main movie={props.movie} />;
+};
+
+App.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  })
 };
 
 export default App;
